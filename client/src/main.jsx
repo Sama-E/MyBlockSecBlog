d7e707aa14c60581e989c46a832cce6e99510f9b
Sama-E <112719147+Sama-E@users.noreply.github.com>
2024-12-28 15:30:46 -0500

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,51 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import './index.css'
+import App from './App.jsx'
+import Homepage from "./routes/Homepage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import Write from "./routes/Write.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
+import RegisterPage from "./routes/RegisterPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/posts",
+        element: <PostListPage />,
+      },
+      {
+        path: "/:slug",
+        element: <SinglePostPage />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
